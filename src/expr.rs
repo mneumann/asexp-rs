ba455dd2f36a43e886b93f1416377da6a47e71b4
Michael Neumann <mneumann@ntecs.de>
2015-12-22 19:54:51 +0100

    Implement Display for Expr

--- src/expr.rs
+++ src/expr.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use super::atom::Atom;
 
 #[derive(Debug, PartialEq)]
@@ -5,11 +6,49 @@ pub enum Expr {
     Atom(Atom),
 
     // ( ... )
-    Tuple(Vec<Atom>),
+    Tuple(Vec<Expr>), /* [ ... ]
+                       * Array(Vec<Atom>),
+                       *
+                       * { key val ... }
+                       * Map(Vec<(Atom, Atom)>),
+                       * */
+}
 
-    // [ ... ]
-    Array(Vec<Atom>),
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match *self {
+            Expr::Atom(ref a) => write!(f, "{}", a),
+            Expr::Tuple(ref t) => {
+                try!(write!(f, "("));
+                for (i, expr) in t.iter().enumerate() {
+                    let space = if i > 0 {
+                        " "
+                    } else {
+                        ""
+                    };
+                    try!(write!(f, "{}{}", space, expr));
+                }
+                write!(f, ")")
+            }
+        }
+    }
+}
 
-    // { key val ... }
-    Map(Vec<(Atom, Atom)>),
+
+#[test]
+fn test_display() {
+    assert_eq!("12345", &format!("{}", Expr::Atom(Atom::UInt(12345))));
+    assert_eq!("(12345)",
+               &format!("{}", Expr::Tuple(vec![Expr::Atom(Atom::UInt(12345))])));
+    assert_eq!("(12345 abc)",
+               &format!("{}",
+                        Expr::Tuple(vec![Expr::Atom(Atom::UInt(12345)),
+                                         Expr::Atom(Atom::Str("abc".to_string()))])));
+    assert_eq!("(12345 (abc))",
+               &format!("{}",
+                        Expr::Tuple(vec![Expr::Atom(Atom::UInt(12345)),
+                                         Expr::Tuple(vec![
+                                         Expr::Atom(Atom::Str("abc".to_string()))
+                                         ])])));
+
 }
