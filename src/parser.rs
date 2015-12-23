862730f010de94146360d0b21fa7d790cc1b12b8
Michael Neumann <mneumann@ntecs.de>
2015-12-23 13:15:29 +0100

    Refactor the parser so that it does not require to peek.
    Return the failed token or error code

--- src/parser.rs
+++ src/parser.rs
@@ -3,75 +3,49 @@ use super::Atom;
 use super::token::{Token, Tokenizer};
 
 pub struct Parser<'a> {
-    current_token: Option<Token<'a>>,
     tokenizer: Tokenizer<'a>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ParseError<'a> {
+    UnexpectedToken(Token<'a>),
+    UnexpectedEnd,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(s: &'a str) -> Parser<'a> {
-        Parser {
-            current_token: None,
-            tokenizer: Tokenizer::new(s, true),
-        }
-    }
-
-    // skips the current token
-    fn skip(&mut self) {
-        assert!(self.current_token.is_some());
-        self.current_token = None;
-    }
-
-    // returns the current token.
-    fn token(&mut self) -> Option<Token<'a>> {
-        if let Some(token) = self.current_token {
-            return Some(token);
-        }
-        assert!(self.current_token.is_none());
-        self.current_token = self.tokenizer.next();
-        self.current_token
+        Parser { tokenizer: Tokenizer::new(s, true) }
     }
 
     pub fn at_end(&mut self) -> bool {
-        self.token().is_none()
+        self.tokenizer.next().is_none()
     }
 
     // Returns either the successfully parsed expression, or
     // the invalid token which was not expected.
-    pub fn parse_expr(&mut self) -> Result<Expr, Token<'a>> {
-        let current = self.token().unwrap();
-        match current {
-            Token::Str(s) => {
-                self.skip();
-                Ok(Expr::from(s))
-            }
-            Token::UInt(u) => {
-                self.skip();
-                Ok(Expr::from(Atom::UInt(u)))
-            }
-            Token::SInt(s) => {
-                self.skip();
-                Ok(Expr::from(Atom::SInt(s)))
-            }
-            Token::Float(s) => {
-                self.skip();
-                Ok(Expr::from(Atom::Float(s)))
-            }
-            Token::OpenBrace => {
-                self.skip();
-                let mut exprs = vec![];
-                loop {
-                    if let Some(Token::CloseBrace) = self.token() {
-                        self.skip();
-                        break;
+    pub fn parse_expr(&mut self) -> Result<Expr, ParseError<'a>> {
+        match self.tokenizer.next() {
+            None => Err(ParseError::UnexpectedEnd),
+            Some(tok) => {
+                match tok {
+                    Token::Str(s) => Ok(Expr::from(s)),
+                    Token::UInt(u) => Ok(Expr::from(Atom::UInt(u))),
+                    Token::SInt(s) => Ok(Expr::from(Atom::SInt(s))),
+                    Token::Float(s) => Ok(Expr::from(Atom::Float(s))),
+                    Token::OpenBrace => {
+                        let mut exprs = vec![];
+                        loop {
+                            match self.parse_expr() {
+                                Ok(ex) => exprs.push(ex),
+                                Err(ParseError::UnexpectedToken(Token::CloseBrace)) => break,
+                                Err(err) => return Err(err),
+                            }
+                        }
+                        Ok(Expr::Tuple(exprs))
                     }
-                    match self.parse_expr() {
-                        Ok(ex) => exprs.push(ex),
-                        Err(tok) => return Err(tok),
-                    }
+                    tok => Err(ParseError::UnexpectedToken(tok)),
                 }
-                Ok(Expr::Tuple(exprs))
             }
-            tok => Err(tok),
         }
     }
 }
@@ -97,4 +71,14 @@ fn test_parse_expr() {
     let mut p = Parser::new("(test 123)   123");
     assert_eq!(Ok(Expr::from(("test", 123usize))), p.parse_expr());
     assert_eq!(false, p.at_end());
+
+    let mut p = Parser::new("   ");
+    assert_eq!(Err(ParseError::UnexpectedEnd), p.parse_expr());
+
+    let mut p = Parser::new("(abc    ");
+    assert_eq!(Err(ParseError::UnexpectedEnd), p.parse_expr());
+
+    let mut p = Parser::new("(abc  }  ");
+    assert_eq!(Err(ParseError::UnexpectedToken(Token::CloseCurly)),
+               p.parse_expr());
 }
